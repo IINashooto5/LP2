5baeb771a0b801f0f5cb347960aecbf8f2b078ea
IINashooto <41540268+IINashooto@users.noreply.github.com>
2018-08-26 00:43:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1504,7 +1504,7 @@ msg.channel.send(embed).then(() => {
 
 client.on('ready', function(){	
     var ms = 40000 ;	
-    var setGame = ['-help'];	
+    var setGame = ['tybe -help'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
