8f18783c1d662f66fe99c7b9e7c1f6ee75ef713c
IINashooto <41540268+IINashooto@users.noreply.github.com>
2018-08-26 00:55:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,32 @@ const sql = require("sqlite");
 ,spee={};
 
 
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame(` | -help | `,"https://www.twitch.tv/")
+    console.log('')
+    console.log('')
+    console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+    console.log(`[Start] ${new Date()}`);
+    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════════════════════════════]╗');
+    console.log(`Logged in as * [ " ${client.user.username} " ]`);
+    console.log('')
+    console.log('Informations :')
+    console.log('')
+    console.log(`servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Users! [ " ${client.users.size} " ]`);
+    console.log(`channels! [ " ${client.channels.size} " ]`);
+    console.log('╚[════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════]╗')
+    console.log(' Bot Is Online')
+    console.log('╚[════════════]╝')
+    console.log('')
+    console.log('')
+  });
+
 
 var guilds = {};
 client.on('guildBanAdd', function(guild) {
@@ -1500,15 +1526,6 @@ msg.channel.send(embed).then(() => {
     spee[id] = (new Date).getTime()
 }
 });
-
-
-client.on('ready', function(){	
-    var ms = 40000 ;	
-    var setGame = ['-help'];	
-    var i = -1;	
-    var j = 0;	
-});
-
 
 client.on('message', message => {
   var prefix ="-"; 
