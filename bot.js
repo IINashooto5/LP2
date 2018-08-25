5d1134fc505a94432457f098c535534427fb0d4e
IINashooto <41540268+IINashooto@users.noreply.github.com>
2018-08-26 00:52:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1504,20 +1504,9 @@ msg.channel.send(embed).then(() => {
 
 client.on('ready', function(){	
     var ms = 40000 ;	
-    var setGame = ['tybe -help'];	
+    var setGame = ['-help'];	
     var i = -1;	
     var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
 });
 
 
