c5291bec5d87cc7ea6d719d9222c7a1e131f86f0
IINashooto <41540268+IINashooto@users.noreply.github.com>
2018-08-26 00:38:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1504,7 +1504,7 @@ msg.channel.send(embed).then(() => {
 
 client.on('ready', function(){	
     var ms = 40000 ;	
-    var setGame = ['-inv','-help'];	
+    var setGame = ['-help'];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
