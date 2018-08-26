be98b53335866bc0374241c13a84ee497e5bd42a
IINashooto <41540268+IINashooto@users.noreply.github.com>
2018-08-26 15:16:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -140,6 +140,26 @@ rebellog.send(`<@!${channelremover.id}>
 حآول العبث بالسيرفر @everyone`);
 channel.guild.owner.send(`<@!${channelremover.id}>
 حآول العبث بالسيرفر ${channel.guild.name}`)
+
+client.on('message', message => {
+    if (message.content.startsWith("رابط")) {
+  
+  message.channel.createInvite({
+        thing: true,
+        maxUses: 5,
+        maxAge: 86400
+    }).then(invite =>
+      message.author.sendMessage(invite.url)
+    )
+  message.channel.send("**تم ارسال الرابط برسالة خاصة**")
+  
+  message.author.send(`**مدة الرابط : يـوم
+  عدد استخدامات الرابط : 5**`)
+  
+  
+    }
+  });
+  
 }
   setTimeout(() => {
  channelr[channelremover.id].deleted = 0;
